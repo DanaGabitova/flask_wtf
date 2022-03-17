2a7e5edeabc79970534cac9ebf560f8d1f61bf61
DanaGabitova <58876248+DanaGabitova@users.noreply.github.com>
2022-03-17 11:04:06 +0600

    решение задачи 2 из урока яндекс лицея

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    name = "И на Марсе будут яблони цвести!"
+    return render_template('index.html', title='Индекс страница',
+                           mission=name)
+
+
+@app.route('/training/<prof>')
+def prof_training(prof):
+    prof = prof.lower()
+    if "инженер" in prof or "строитель" in prof:
+        profession = "Инженерные тренажёры"
+        image = "ing_schip..jpg"
+    else:
+        profession = "Научные симуляторы"
+        image = "scn_schip.jpg"
+    return render_template('training.html',
+                           profession=profession,
+                           image=url_for('static', filename='img/' + image))
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1', debug=True)
