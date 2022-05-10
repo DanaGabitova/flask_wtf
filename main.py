5eda5fa7a8f1c8b4e30f3a300803c2acb65bdefa
DanaGabitova <58876248+DanaGabitova@users.noreply.github.com>
2022-05-10 13:48:27 +0300

    Решение к 6 задаче

--- main.py
+++ main.py
@@ -54,5 +54,10 @@ def login():
                            form=form)
 
 
+@app.route('/distribution')
+def distribution():
+    return render_template('distribution.html', astronauts=astronauts)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1', debug=True)
